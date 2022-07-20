5c6a7a41baa72b27f0b33229fc17e498bf6f2ef9
minxd95 <newobj95@gmail.com>
2022-07-20 17:51:23 +0900

    firebase 모듈화
    완료 하였으나 provider간의 의존성 문제로 firebase 관련 로직은 lib로 빼야할 것 같음..

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TestsModule } from './tests/tests.module';
+import { FirebaseModule } from './firebase/firebase.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TestsModule,
+    FirebaseModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/tests/tests.controller.ts
@@ -0,0 +1,46 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
+import { TestsService } from './tests.service';
+import { CreateTestDto } from './dto/create-test.dto';
+import { UpdateTestDto } from './dto/update-test.dto';
+import { DatabaseService } from 'src/firebase/database/database.service';
+
+@Controller('tests')
+export class TestsController {
+  constructor(
+    private readonly testsService: TestsService,
+    private readonly databaseService: DatabaseService,
+  ) {}
+
+  @Post()
+  create(@Body() createTestDto: CreateTestDto) {
+    return this.testsService.create(createTestDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.testsService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.testsService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updateTestDto: UpdateTestDto) {
+    return this.testsService.update(+id, updateTestDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.testsService.remove(+id);
+  }
+}
